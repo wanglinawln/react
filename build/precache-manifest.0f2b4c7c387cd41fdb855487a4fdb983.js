4d5fface70d0f598e0a74bd344d8d6d118de695c
wanglinawln <1416978302@qq.com>
2019-10-14 15:41:42 +0800

    build

---
+++ build/precache-manifest.0f2b4c7c387cd41fdb855487a4fdb983.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687f10f5dc7aced7e8770608bbd4a6b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18c7874533a9e6c1787d",
+    "url": "/static/css/main.1d96b853.chunk.css"
+  },
+  {
+    "revision": "c68dd5ff664cf1457f94",
+    "url": "/static/js/2.a10b181d.chunk.js"
+  },
+  {
+    "revision": "18c7874533a9e6c1787d",
+    "url": "/static/js/main.acffe72c.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
